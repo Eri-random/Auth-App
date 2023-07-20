76e5a5c92e939425f4bf242c9658ca93cd3c77a2
Eri-random <brenherrera@alumno.unlam.edu.ar>
2023-07-20 09:38:40 -0300

    post login

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,24 +1,42 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, inject, signal } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Injectable, inject, signal, computed } from '@angular/core';
+import { map, Observable, of,tap } from 'rxjs';
 import { environment } from '../../../environments/environments';
-import { AuthStatus, User } from '../interfaces';
+import { AuthStatus, LoginResponse, User } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  private readonly baseIrl: string =environment.baseUrl;
+  private readonly baseUrl: string =environment.baseUrl;
   private http = inject(HttpClient)
 
   private _currentUser = signal<User | null>(null);
   private _authStatus = signal<AuthStatus>(AuthStatus.checking);
 
+  public currentUser = computed(() => this._currentUser);
+  public authStatus = computed(() => this._authStatus);
+
   constructor() { }
 
   login(email:string, password:string): Observable<Boolean>{
 
-    return of(true);
+    const url = `${this.baseUrl}/auth/login`;
+    const body = {email, password}
+
+    return this.http.post<LoginResponse>(url,body)
+      .pipe(
+        tap(({user,token}) =>{
+          this._currentUser.set(user);
+          this._authStatus.set(AuthStatus.authenticated);
+          localStorage.setItem('token',token)
+          console.log({user,token});
+        }),
+        map(() => true)
+    );
+
+    //Tdo: errores
+
   }
 }
