0ddaa513f6ceed4e6a7fdadd953d27f31d7c5edb
Eri-random <brenherrera@alumno.unlam.edu.ar>
2023-08-18 14:34:55 -0300

    manejo de errores / token de acceso / guards

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,8 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable, inject, signal, computed } from '@angular/core';
-import { map, Observable, of,tap } from 'rxjs';
+import { catchError, map, Observable, of,tap, throwError } from 'rxjs';
 import { environment } from '../../../environments/environments';
 import { AuthStatus, LoginResponse, User } from '../interfaces';
+import { CheckTokenResponse } from '../interfaces/check-token.response';
 
 @Injectable({
   providedIn: 'root'
@@ -15,28 +16,48 @@ export class AuthService {
   private _currentUser = signal<User | null>(null);
   private _authStatus = signal<AuthStatus>(AuthStatus.checking);
 
-  public currentUser = computed(() => this._currentUser);
-  public authStatus = computed(() => this._authStatus);
+  public currentUser = computed(() => this._currentUser());
+  public authStatus = computed(() => this._authStatus());
 
   constructor() { }
 
-  login(email:string, password:string): Observable<Boolean>{
+  private setAuthentication(user:User, token:string):boolean{
+    this._currentUser.set(user);
+    this._authStatus.set(AuthStatus.authenticated);
+    localStorage.setItem('token',token)
+    return true;
+  }
+
+  login(email:string, password:string): Observable<boolean>{
 
     const url = `${this.baseUrl}/auth/login`;
     const body = {email, password}
 
     return this.http.post<LoginResponse>(url,body)
       .pipe(
-        tap(({user,token}) =>{
-          this._currentUser.set(user);
-          this._authStatus.set(AuthStatus.authenticated);
-          localStorage.setItem('token',token)
-          console.log({user,token});
-        }),
-        map(() => true)
+        map(({user,token}) => this.setAuthentication(user,token)),
+        catchError(err => throwError(() => err.error.message))
     );
 
-    //Tdo: errores
+  }
 
+  checkAuthStatus():Observable<boolean>{
+
+    const url = `${this.baseUrl}/auth/check-token`;
+    const token = localStorage.getItem('token');
+
+    if(!token) return of(false);
+
+    const headers = new HttpHeaders()
+    .set('Authorization',`Bearer ${token}`)
+
+    return this.http.get<CheckTokenResponse>(url,{headers})
+      .pipe(
+          map(({token,user})=> this.setAuthentication(user,token)),
+          catchError(() => {
+          this._authStatus.set(AuthStatus.notAuthenticated);
+          return of(false)
+        })
+      );
   }
 }
--- src/app/dashboard/layouts/dashboard-layout/dashboard-layout.component.ts
+++ src/app/dashboard/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject, computed } from '@angular/core';
+import { AuthService } from '../../../auth/services/auth.service';
 
 @Component({
   templateUrl: './dashboard-layout.component.html',
   styleUrls: ['./dashboard-layout.component.css']
 })
 export class DashboardLayoutComponent {
+
+  private authService = inject(AuthService);
+
+  public user = computed(() => this.authService.currentUser());
 
 }
